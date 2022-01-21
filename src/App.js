25a0f0e2952274d7beba75b6f87b60d80d0937e6
nKarad <n.karad@gmail.com>
2022-01-21 20:38:38 -0300

    Nav

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
+
+
 
 const productoRebaja = "Jerseys de temporadas pasadas"
 const porcentajeRebaja = "40%"
 const compraMinima = 22;
 
+
+
+
 function App() {
   return (
+
+ 
     <div className="App">
-
-
+      <NavBar/>
 
       <h1>E-commerce del K</h1>
-      <p>Ofertas del mes: Rebajas en {productoRebaja} del {productoRebaja} compra minima {compraMinima}}</p>
+      <p>Ofertas del mes: Rebajas en {productoRebaja}
+       del {porcentajeRebaja} compra minima {compraMinima}</p>
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+const NavBar = () => {
+    return (
+
+<nav>
+    <ul>
+        <li>iotem</li>
+        <li>iotem</li>
+        <li>iotem</li>
+        <li>iotem</li>
+    </ul>
+</nav>
+);
+};
+
+export default NavBar
