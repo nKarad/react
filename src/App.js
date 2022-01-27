ad224e2c07dea9ebdd96d2f46f5557265086ac10
nKarad <n.karad@gmail.com>
2022-01-26 23:09:43 -0300

    3rd commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import'./components/NavBar.css';
 import NavBar from './components/NavBar';
 
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,19 @@
+import React from 'react';
+import'./NavBar.css';
+import logo from './logo.png'
+
 const NavBar = () => {
     return (
 
-<nav>
-    <ul>
-        <li>iotem</li>
-        <li>iotem</li>
-        <li>iotem</li>
-        <li>iotem</li>
-    </ul>
+<nav className='Navigation'>
+
+<a href='index.html'><img src={logo} alt='logo' /></a>
+<ul>
+    
+    <li class="inicio"><a href="index.html">Tienda</a></li>
+    <li class="inicio"><a href="promociones.html">Promociones</a></li>
+    <li class="inicio"><a href="contacto.html">Contactanos</a></li>
+</ul>
 </nav>
 );
 };
